700458b2d46478388be9580227d7cc1dcc9ac7fc
um4d <timur.simonov@gmail.com>
2016-11-14 15:03:42 +0300

    Training excercises

--- week 2/Training/src/ResizingArrayStackOfStrings.java
+++ week 2/Training/src/ResizingArrayStackOfStrings.java
@@ -2,8 +2,31 @@
  * Created by t.simonov on 11.11.16.
  */
 public class ResizingArrayStackOfStrings {
+    String [] s;
+    int N = 0;
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+    public void push(String item) {
+        if (N == s.length) {
+            resize(2 * s.length);
+            s[N++] = item;
+        }
+    }
+    public String pop() {
+        String item = s[--N];
+        s[N] = null;
+        if (N > 0 && N == s.length/4) resize(s.length/2);
+        return item;
+    }
 
-    s = new String[1]
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
+    }
 
 }
 
