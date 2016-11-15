b9aea5e1fdb2d02b05712ffb59806d094414979a
Umad Umad <Umad Umad>
2016-11-15 22:54:41 +0300

    deque v.0.2

--- week 2/Deque/src/Deque.java
+++ week 2/Deque/src/Deque.java
@@ -5,39 +5,70 @@ import java.util.function.Consumer;
 /**
  * Created by t.simonov on 10.11.16.
  */
-public class Deque<Item> implements Iterable<Item>{
+public class Deque<Item> implements Iterable<Item> {
 
     private Node first, last;
     private int dequeSize;
 
 
     public Deque() {
-
+        first = null;
+        last = null;
+        dequeSize = 0;
     }
+
     public boolean isEmpty() {
         return first == null || last == null;
     }
+
     public int size() {
         return dequeSize;
     }
+
     public void addFirst(Item item) {
         validateItem(item);
-        dequeSize++;
-        if (isEmpty()) {
-            first = last;
-        }
+        ++dequeSize;
         Node oldFirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldFirst;
+        first.previous = null;
+        if (isEmpty()) {
+            last = first;
+        } else oldFirst.previous = first;
     }
+
     public void addLast(Item item) {
-
+        validateItem(item);
+        ++dequeSize;
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.previous = oldLast;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else oldLast.next = last;
     }
+
     public Item removeFirst() {
-
+        validateDequeIsEmpty();
+        Node oldFirst = first;
+        first = first.next;
+        Item item = oldFirst.item;
+        oldFirst.next = null;
+        --dequeSize;
+        return item;
     }
-    public Item removeLast() {
 
+    public Item removeLast() {
+        validateDequeIsEmpty();
+        Node oldLast = last;
+        last = last.previous;
+        Item item = oldLast.item;
+        oldLast.previous = null;
+        --dequeSize;
+        return item;
     }
 
     private class Node {
@@ -47,27 +78,20 @@ public class Deque<Item> implements Iterable<Item>{
     }
 
     private void validateItem(Item item) {
-        if(item == null) {
+        if (item == null) {
             throw new java.lang.NullPointerException();
         }
     }
 
+    private void validateDequeIsEmpty() {
+        if (isEmpty()) {
+            throw new java.lang.NullPointerException();
+        }
+    }
 
 
     @Override
     public Iterator<Item> iterator() {
         return null;
     }
-
-    @Override
-    public void forEach(Consumer<? super Item> action) {
-
-    }
-
-    @Override
-    public Spliterator<Item> spliterator() {
-        return null;
-    }
 }
-
-
