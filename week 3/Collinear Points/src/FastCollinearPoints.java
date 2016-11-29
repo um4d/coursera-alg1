166e4b3c167a3856a16ccf2d3018af5c8b19099a
um4d <timur.simonov@gmail.com>
2016-11-29 18:10:11 +0300

    not working

--- week 3/Collinear Points/src/FastCollinearPoints.java
+++ week 3/Collinear Points/src/FastCollinearPoints.java
@@ -18,22 +18,47 @@ public class FastCollinearPoints {
 
         for (int i = 0; i < len; i++) {
 
+            System.out.println("----------------------------");
+            System.out.println("P = " + i);
             Point p = points[i];
             Arrays.sort(copyPoints, p.slopeOrder());
-            int count = 2;
-            int f = 1;
+            int count = 1;
+            Point[] monoSlope = new Point[len];
+            monoSlope[0] = p;
+
 
             for (int j = 1; j < len - 1; j++) {
+                monoSlope[1] = copyPoints[j];
+                System.out.println("p -> " + (j + 1) + " " + p.slopeTo(copyPoints[j + 1]) + " p -> " + j + " " + p.slopeTo(copyPoints[j]));
                 if (p.slopeTo(copyPoints[j + 1]) == p.slopeTo(copyPoints[j])) {
+                    monoSlope[count + 1] = copyPoints[j + 1];
                     count++;
-                    if (j == len - 2 && count > 3) {
-                        lineSegments.add(new LineSegment(copyPoints[j - 1], copyPoints[j + count - 2]));
+                } else if (count > 2) {
+                    Point[] trueMonoSlope = new Point[count + 1];
+                    System.arraycopy(monoSlope, 0, trueMonoSlope, 0, trueMonoSlope.length);
+                    Arrays.sort(trueMonoSlope);
+                    LineSegment line = new LineSegment(trueMonoSlope[0], trueMonoSlope[count]);
+                    count = 1;
+                    if (!lineEqual(line)) {
+                        lineSegments.add(line);
+                        break;
                     }
-                } else if (count > 3) {
-                    lineSegments.add(new LineSegment(copyPoints[j - 1], copyPoints[j + count - 2]));
-                    j = j + count - 2;
-                    count = 2;
+
                 }
+
+//                    if (j == len - 2 && count > 3) {
+//                        lineSegments.add(new LineSegment(copyPoints[j - 1], copyPoints[j + count - 2]));
+//                    }
+//                } else {
+//                    Arrays.sort(copyPoints, f, f + count);
+//                    LineSegment line = new LineSegment(copyPoints[f], copyPoints[f + count - 1]);
+//                    if (count > 3 && !lineEqual(line) ) {
+//                        lineSegments.add(line);
+//                        j = f + count - 2;
+//                        count = 1;
+//                    }
+//                    f = j;
+//                }
             }
         }
     }
