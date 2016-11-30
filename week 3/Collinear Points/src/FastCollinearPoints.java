c8d66d2f790c573a79e033c4b1992c1d2be844e7
um4d <timur.simonov@gmail.com>
2016-11-30 17:55:28 +0300

    its done

--- week 3/Collinear Points/src/FastCollinearPoints.java
+++ week 3/Collinear Points/src/FastCollinearPoints.java
@@ -13,63 +13,22 @@ public class FastCollinearPoints {
 
         int len = points.length;
         Arrays.sort(points);
-        Point[] copyPoints = new Point[len];
-        System.arraycopy(points, 0, copyPoints, 0, len);
+        Point[] copyPoints = points.clone();
 
-        for (int i = 0; i < len; i++) {
-
-            System.out.println("----------------------------");
-            System.out.println("P = " + i);
+        for (int i = 0; i < len - 3; i++) {
             Point p = points[i];
+            Arrays.sort(copyPoints);
             Arrays.sort(copyPoints, p.slopeOrder());
-            int count = 1;
-            Point[] monoSlope = new Point[len];
-            monoSlope[0] = p;
-
-
-            for (int j = 1; j < len - 1; j++) {
-                monoSlope[1] = copyPoints[j];
-                System.out.println("p -> " + (j + 1) + " " + p.slopeTo(copyPoints[j + 1]) + " p -> " + j + " " + p.slopeTo(copyPoints[j]));
-                if (p.slopeTo(copyPoints[j + 1]) == p.slopeTo(copyPoints[j])) {
-                    monoSlope[count + 1] = copyPoints[j + 1];
-                    count++;
-                } else if (count > 2) {
-                    Point[] trueMonoSlope = new Point[count + 1];
-                    System.arraycopy(monoSlope, 0, trueMonoSlope, 0, trueMonoSlope.length);
-                    Arrays.sort(trueMonoSlope);
-                    LineSegment line = new LineSegment(trueMonoSlope[0], trueMonoSlope[count]);
-                    count = 1;
-                    if (!lineEqual(line)) {
-                        lineSegments.add(line);
-                        break;
-                    }
-
+            for (int q = 1, r = 2; r < len; r++) {
+                while( r < len && p.slopeTo(copyPoints[q]) == p.slopeTo(copyPoints[r])) {
+                    r++;
                 }
-
-//                    if (j == len - 2 && count > 3) {
-//                        lineSegments.add(new LineSegment(copyPoints[j - 1], copyPoints[j + count - 2]));
-//                    }
-//                } else {
-//                    Arrays.sort(copyPoints, f, f + count);
-//                    LineSegment line = new LineSegment(copyPoints[f], copyPoints[f + count - 1]);
-//                    if (count > 3 && !lineEqual(line) ) {
-//                        lineSegments.add(line);
-//                        j = f + count - 2;
-//                        count = 1;
-//                    }
-//                    f = j;
-//                }
+                if ((r - q >= 3) && p.compareTo(copyPoints[q]) < 0) {
+                    lineSegments.add(new LineSegment(p, copyPoints[r - 1]));
+                }
+                q = r;
             }
         }
-    }
-
-    private boolean lineEqual(LineSegment line) {
-        for (LineSegment segment : lineSegments) {
-            if (segment.toString().equals(line.toString())) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public int numberOfSegments() {
